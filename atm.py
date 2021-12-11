d8def9269cf220d5ebd32fe59dfdb061591a9769
Soumili1234 <74143945+Soumili123@users.noreply.github.com>
2021-12-11 18:01:46 +0530

    Add files via upload

---
+++ atm.py
@@ -0,0 +1,13 @@
+class Atm(object):
+
+    def _init_(self,atmcardno, pinno): 
+        self.atmcardno = atmcardno
+        self.pinno = pinno
+
+        def balanceEnquiry(self):
+         print("Balance Enquiry")
+
+        def cashWithdrawal(self):
+         print("Cash Withdrawal")
+
+         
